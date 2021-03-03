692c104b379651d9b25ab9b4a5716719539d3f5a
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2021-03-03 20:21:45 -0300

    Primeiro commit nlw

---
+++ src/components/CompleteChallenge.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/components/CompleteChallenge.module.css';
+
+export function CompleteChallenge() {
+    return (
+        <div className={styles.completeChallengeContainer}>
+            <span> Desafios completos </span>
+            <span>5</span>
+
+
+        </div>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import styles from "../styles/pages/Home.module.css";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { CompleteChallenge } from "../components/CompleteChallenge"
+import { Countdown } from "../components/Countdown";
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+
+    <div className={styles.container}>
+      <Head>
+
+        <title>Inicio | Move.it</title>
+      </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompleteChallenge />
+          <Countdown />
+        </div>
+
+
+      </section>
+
+    </div>
+  )
+}
